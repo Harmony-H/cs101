7dc48cda9f961a3cfc984ff64b41bdbe0ee9c5a0
Harmony-H <harmonyhuang1130@gmail.com>
2025-10-02 13:32:41 +0800

    Create main1.c

---
+++ EX3/main1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void print_sp(int i, int n){
+    for(;i<n;i++){
+        printf(" ");
+    }
+}
+
+void print_num(int n){
+    for(int i = 1;i<=n;i++){
+        printf("%d ",n);
+    }
+    printf("\n");
+}
+
+int main(){
+    int rows = 6;
+    for(int i = 1;i<=rows;i++){
+        print_sp(i,rows);
+        print_num(i);
+    }
+    return 0;
+}
